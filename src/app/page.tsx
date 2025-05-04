694ea2e37dbea437530e4e44eaf2d1049d950114
Tomas Vergara <tominacho@hotmail.com>
2025-05-04 19:19:29 -0300

    refactor: remove unused SVG files and update page metadata for improved clarity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 // pages/page.tsx
 import Canvas from "@/components/Canvas/Canvas";
+import { icons } from "lucide-react";
+
+export const metadata = {
+  title: "portfolio - tv",
+  description:
+    "a procedurally generated analog photography portfolio by tomás vergara, systems engineering student and film enthusiast.",
+};
 
 export default function HomePage() {
   return (
