304feca3b8b90f01eaac93ee64d316b58005bccc
Tomas Vergara <tominacho@hotmail.com>
2025-05-04 18:46:33 -0300

    refactor: added hover card for information and ping animation on about me

--- src/components/BottomBar.tsx
+++ src/components/BottomBar.tsx
@@ -4,12 +4,12 @@ import { useCanvas } from "./CanvasContext";
 import { motion, AnimatePresence } from "framer-motion";
 import { Button } from "@/components/ui/button";
 import {
-  Search,
   Target,
   ArrowLeft,
   ArrowRight,
   FileUser,
   Undo2,
+  Info,
 } from "lucide-react";
 
 import Link from "next/link";
@@ -20,15 +20,23 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
+
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card";
+
 import { Separator } from "./ui/separator";
 import { AboutMe } from "./AboutMe";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export function BottomBar() {
   const { currentRollIndex, rollsCount, centerOnRoll, setCurrentRollIndex } =
     useCanvas();
 
   const [showAbout, setShowAbout] = useState(false);
+  const [showNotification, setShowNotification] = useState(false);
 
   const handleMoveForward = () => {
     if (rollsCount > 0) {
@@ -46,6 +54,20 @@ export function BottomBar() {
     centerOnRoll(currentRollIndex);
   };
 
+  useEffect(() => {
+    const hasVisitedAbout = localStorage.getItem("hasVisitedAbout");
+    setShowNotification(!hasVisitedAbout);
+  }, []);
+
+  const handleAboutClick = () => {
+    if (!showAbout) {
+      // Cuando el usuario abre el About Me, guardamos en localStorage
+      localStorage.setItem("hasVisitedAbout", "true");
+      setShowNotification(false);
+    }
+    setShowAbout(!showAbout);
+  };
+
   return (
     <>
       <AboutMe isVisible={showAbout} onClose={() => setShowAbout(false)} />
@@ -57,10 +79,18 @@ export function BottomBar() {
                 <Button
                   size="icon"
                   asChild
-                  className="hover:bg-neutral-600/20  rounded-none"
-                  onClick={() => setShowAbout(!showAbout)}
+                  className="hover:bg-neutral-600/20 bg-black/0 rounded-none relative" // Añade 'relative' aquí
+                  onClick={() => handleAboutClick()}
                 >
-                  <Link href="/">{showAbout ? <Undo2 /> : <FileUser />}</Link>
+                  <Link href="/">
+                    {showAbout ? <Undo2 /> : <FileUser />}
+                    {/* Elemento de ping/anotación */}
+                    {showNotification && (
+                      <span className="absolute h-5 w-5">
+                        <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-neutral-400 opacity-75"></span>
+                      </span>
+                    )}
+                  </Link>
                 </Button>
               </TooltipTrigger>
               <TooltipContent className="mb-2">
@@ -86,7 +116,7 @@ export function BottomBar() {
                       <Button
                         size="icon"
                         asChild
-                        className="hover:bg-neutral-600/20 rounded-none"
+                        className="hover:bg-neutral-600/20 bg-black/0 rounded-none"
                         onClick={handleMoveBackward}
                       >
                         <Link href="/">
@@ -103,7 +133,7 @@ export function BottomBar() {
                       <Button
                         size="icon"
                         asChild
-                        className="hover:bg-neutral-600/20 hover:animate-pulse rounded-none"
+                        className="hover:bg-neutral-600/20 bg-black/0 hover:animate-pulse rounded-none"
                         onClick={handleCenter}
                       >
                         <Link href="/">
@@ -120,7 +150,7 @@ export function BottomBar() {
                       <Button
                         size="icon"
                         asChild
-                        className="hover:bg-neutral-600/20 rounded-none"
+                        className="hover:bg-neutral-600/20 bg-black/0 rounded-none"
                         onClick={handleMoveForward}
                       >
                         <Link href="/">
@@ -139,20 +169,52 @@ export function BottomBar() {
           </AnimatePresence>
           <TooltipProvider>
             <Tooltip>
-              <TooltipTrigger asChild>
-                <Button
-                  size="icon"
-                  className="hover:bg-neutral-600/20  rounded-none"
-                  asChild
-                >
-                  <Link href="/">
-                    <Search />
-                  </Link>
-                </Button>
-              </TooltipTrigger>
+              <TooltipTrigger asChild></TooltipTrigger>
               <TooltipContent className="mb-2">search</TooltipContent>
             </Tooltip>
           </TooltipProvider>
+
+          <HoverCard openDelay={200} closeDelay={0}>
+            <HoverCardTrigger asChild>
+              <Button
+                size="icon"
+                className="hover:bg-neutral-600/20 bg-black/0  rounded-none"
+                asChild
+              >
+                <Link href="/">
+                  <Info />
+                </Link>
+              </Button>
+            </HoverCardTrigger>
+            <HoverCardContent sideOffset={15}>
+              <div className="prose prose-invert prose-neutral">
+                <h4 className="text-lg font-serif">did you know?</h4>
+                <p className="lowercase text-justify text-sm">
+                  This portfolio is meant to represent how these photos live in
+                  my mind:{" "}
+                  <i>
+                    <b>
+                      organized, yet a little chaotic. Structured, but organic.
+                    </b>
+                  </i>{" "}
+                  Just a collection of images with strong meaning, floating
+                  freely through infinite space.
+                </p>
+                <p className="lowercase text-justify text-sm mt-1/2">
+                  To reflect that, the canvas you're seeing is procedurally
+                  generated using a simulation of physical forces. These forces
+                  determine how each image interacts with the others, making the
+                  layout unique to you, in this moment.
+                </p>
+                <Separator className="mt-0 mb-0 h-0.5" />
+                <p className="lowercase text-justify text-sm mt-1/2 italic">
+                  You can use the navigation controls to move between different
+                  rolls, but you're also free to zoom, pan, and explore as much
+                  as you like. Take your time—there's no wrong way to wander.
+                </p>
+              </div>
+            </HoverCardContent>
+          </HoverCard>
         </div>
       </div>
     </>
