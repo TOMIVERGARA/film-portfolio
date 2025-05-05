0de98da6b0c11e82914824a4e85ef5bd287bd551
Tomas Vergara <tominacho@hotmail.com>
2025-05-05 20:54:58 -0300

    refactor: added padding to LoadingScreen component

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -6,7 +6,7 @@ import { useEffect, useState } from "react";
 export const LoadingScreen = ({ progress }: { progress: number }) => {
   return (
     <motion.div
-      className="fixed inset-0 z-[9999] bg-neutral-950 flex items-center justify-center"
+      className="fixed inset-0 z-[9999] bg-neutral-950 flex items-center justify-center p-10"
       initial={{ opacity: 1 }}
       exit={{ opacity: 0 }}
       transition={{ duration: 0.5 }}
